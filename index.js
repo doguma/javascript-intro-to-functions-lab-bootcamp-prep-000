c43997e02e13b37d6511aa73bb39e0eade9b08c8
Hannah Do <hannah.hj.do@gmail.com>
2018-07-18 15:59:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ function logWhisper(string){
   console.log( string.toLowerCase() );
 }
 
-function shout(string){
-  return string.toUpperCase();
+function sayHiToGrandma(string){
+  if (string === string.toLowerCase() ){
+    return "I can't hear you!"
+  }else if ( string === string.toUpperCase() ){
+    return "YES INDEED!";
+  }
+  
 }
