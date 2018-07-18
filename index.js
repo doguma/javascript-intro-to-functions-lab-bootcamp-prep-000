a1c01256325eee7e84b729c0cbe43f1e1e559135
Hannah Do <hannah.hj.do@gmail.com>
2018-07-18 15:57:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ function shout(string){
   return string.toUpperCase();
 }
 
+function whisper(string){
+  return string.toLowerCase();
+}
+
+function shout(string){
+  return string.toUpperCase();
+}
+
+function shout(string){
+  return string.toUpperCase();
+}
+
+function shout(string){
+  return string.toUpperCase();
+}
